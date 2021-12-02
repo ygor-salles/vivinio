c3812c8515baf0c2683a84747a0beb3c41b142e0
unknown <ygorsallesaniceto@gmail.com>
2021-12-01 23:06:22 -0300

    Init orm - typeorm

---
+++ ormconfig.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+if (process.env.NODE_ENV === 'development') {
+  module.exports = {
+    type: 'postgres',
+    host: process.env.BD_HOST,
+    port: +process.env.BD_PORT || 5432,
+    username: process.env.BD_USERNAME,
+    password: process.env.BD_PASSWORD,
+    database: process.env.BD_DATABASE,
+    synchronize: false,
+    migrations: ['src/database/migrations/*.ts'],
+    entities: ['src/entities/*.ts'],
+    cli: {
+      migrationsDir: 'src/database/migrations',
+    },
+  };
+} else if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    type: 'postgres',
+    host: process.env.BD_PROD_HOST,
+    port: +process.env.BD_PROD_PORT || 5432,
+    username: process.env.BD_PROD_USERNAME,
+    password: process.env.BD_PROD_PASSWORD,
+    database: process.env.BD_PROD_DATABASE,
+    synchronize: false,
+    extra: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    migrations: ['build/src/database/migrations/*.js'],
+    entities: ['build/src/entities/*.js'],
+    cli: {
+      migrationsDir: 'build/src/database/migrations',
+    },
+  };
+} else {
+    console.log('NODE_ENV incorreto...');
+}
